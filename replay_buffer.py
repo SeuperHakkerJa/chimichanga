fe03e4a9933fdac44415999b9921883f37321a52
SeuperHakkerJa <jacobxiaochen@sina.com>
2024-12-14 18:36:33 -0500

    update replay buffer to have both states and actions

--- replay_buffer.py
+++ replay_buffer.py
@@ -11,24 +11,8 @@ from torch.utils.data import IterableDataset
 
 
 def episode_len(episode):
-    # subtract -1 because the dummy first transition
-    return next(iter(episode.values())).shape[0] - 1
-
-
-def save_episode(episode, fn):
-    with io.BytesIO() as bs:
-        np.savez_compressed(bs, **episode)
-        bs.seek(0)
-        with fn.open('wb') as f:
-            f.write(bs.read())
-
-
-def load_episode(fn):
-    with fn.open('rb') as f:
-        episode = np.load(f)
-        episode = {k: episode[k] for k in episode.keys()}
-        return episode
-
+    # Add 1 for the first (0th) frame.
+    return next(iter(episode.values())).shape[0]
 
 class ReplayBufferStorage:
     def __init__(self, data_specs, replay_dir):
@@ -53,7 +37,7 @@ class ReplayBufferStorage:
             for spec in self._data_specs:
                 value = self._current_episode[spec.name]
                 episode[spec.name] = np.array(value, spec.dtype)
-            self._current_episode = defaultdict(list)
+                self._current_episode[spec.name] = []
             self._store_episode(episode)
 
     def _preload(self):
@@ -71,11 +55,16 @@ class ReplayBufferStorage:
         self._num_transitions += eps_len
         ts = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
         eps_fn = f'{ts}_{eps_idx}_{eps_len}.npz'
-        save_episode(episode, self._replay_dir / eps_fn)
+        save_path = self._replay_dir / eps_fn
+        with io.BytesIO() as buffer:
+            np.savez_compressed(buffer, **episode)
+            buffer.seek(0)
+            with save_path.open('wb') as f:
+                f.write(buffer.read())
 
 
 class ReplayBuffer(IterableDataset):
-    def __init__(self, replay_dir, max_size, num_workers, nstep,  multistep, 
+    def __init__(self, replay_dir, max_size, num_workers, nstep, multistep,
                  discount, fetch_every, save_snapshot):
         self._replay_dir = replay_dir
         self._size = 0
@@ -84,13 +73,11 @@ class ReplayBuffer(IterableDataset):
         self._episode_fns = []
         self._episodes = dict()
         self._nstep = nstep
+        self._multistep = multistep
         self._discount = discount
         self._fetch_every = fetch_every
         self._samples_since_last_fetch = fetch_every
         self._save_snapshot = save_snapshot
-        self._multistep = multistep
-        print('Loading Data into CPU Memory')
-        self._preload()
 
     def _sample_episode(self):
         eps_fn = random.choice(self._episode_fns)
@@ -98,23 +85,17 @@ class ReplayBuffer(IterableDataset):
 
     def _store_episode(self, eps_fn):
         try:
-            episode = load_episode(eps_fn)
-        except:
-            return False
-        eps_len = episode_len(episode)
-        while eps_len + self._size > self._max_size:
-            early_eps_fn = self._episode_fns.pop(0)
-            early_eps = self._episodes.pop(early_eps_fn)
-            self._size -= episode_len(early_eps)
-            early_eps_fn.unlink(missing_ok=True)
-        self._episode_fns.append(eps_fn)
-        self._episode_fns.sort()
-        self._episodes[eps_fn] = episode
-        self._size += eps_len
+            episode = np.load(eps_fn)
+            episode = {k: episode[k] for k in episode.keys()}
+            self._episode_fns.append(eps_fn)
+            self._episodes[eps_fn] = episode
+            self._size += episode_len(episode)
 
-        if not self._save_snapshot:
-            eps_fn.unlink(missing_ok=True)
-        return True
+            if not self._save_snapshot:
+                eps_fn.unlink(missing_ok=True)
+        except:
+            print(f'Could not load episode: {eps_fn}')
+            print(traceback.format_exc())
 
     def _try_fetch(self):
         if self._samples_since_last_fetch < self._fetch_every:
@@ -135,14 +116,8 @@ class ReplayBuffer(IterableDataset):
             if fetched_size + eps_len > self._max_size:
                 break
             fetched_size += eps_len
-            if not self._store_episode(eps_fn):
-                break
-    
-    def _preload(self):
-        eps_fns = sorted(self._replay_dir.glob('*.npz'), reverse=True)
-        for eps_fn in eps_fns:
             self._store_episode(eps_fn)
-    
+
     def _sample(self):
         try:
             self._try_fetch()
@@ -151,12 +126,9 @@ class ReplayBuffer(IterableDataset):
         self._samples_since_last_fetch += 1
         episode = self._sample_episode()
         # add +1 for the first dummy transition
-        n_step = max(self._nstep, self._multistep)
-        idx = np.random.randint(0, episode_len(episode) - n_step + 1) + 1
+        idx = np.random.randint(0, episode_len(episode) - self._nstep + 1) + 1
         obs = episode['observation'][idx - 1]
-        r_next_obs = episode['observation'][idx + self._multistep - 1]
         action = episode['action'][idx]
-        action_seq = np.concatenate([episode['action'][idx+i][None, :] for i in range(self._multistep)])
         next_obs = episode['observation'][idx + self._nstep - 1]
         reward = np.zeros_like(episode['reward'][idx])
         discount = np.ones_like(episode['discount'][idx])
@@ -164,7 +136,17 @@ class ReplayBuffer(IterableDataset):
             step_reward = episode['reward'][idx + i]
             reward += discount * step_reward
             discount *= episode['discount'][idx + i] * self._discount
-        return (obs, action, action_seq, reward, discount, next_obs, r_next_obs)
+
+        # For TACO: get sequence of actions
+        action_seq = episode['action'][idx:idx + self._multistep]
+
+        # For Chimichanga: get sequence of state-action pairs
+        states_seq = episode['observation'][idx - 1:idx + 1]  # Get current and next state
+        actions_seq = episode['action'][idx:idx + 2]  # Get current and next action
+
+        # Return both formats to support both agents
+        return (obs, action, action_seq, states_seq, actions_seq, 
+                reward, discount, next_obs, episode['observation'][idx + self._nstep])
 
     def __iter__(self):
         while True:
@@ -178,28 +160,21 @@ def _worker_init_fn(worker_id):
 
 
 def make_replay_loader(replay_dir, max_size, batch_size, num_workers,
-                       save_snapshot, nstep, multistep, discount):
+                      save_snapshot, nstep, multistep, discount):
     max_size_per_worker = max_size // max(1, num_workers)
-    
+
     iterable = ReplayBuffer(replay_dir,
-                            max_size_per_worker,
-                            num_workers,
-                            nstep,
-                            multistep,
-                            discount,
-                            fetch_every=1000,
-                            save_snapshot=save_snapshot)
+                          max_size_per_worker,
+                          num_workers,
+                          nstep,
+                          multistep,
+                          discount,
+                          fetch_every=1000,
+                          save_snapshot=save_snapshot)
 
     loader = torch.utils.data.DataLoader(iterable,
-                                         batch_size=batch_size,
-                                         num_workers=num_workers,
-                                         pin_memory=True,
-                                         worker_init_fn=_worker_init_fn)
+                                       batch_size=batch_size,
+                                       num_workers=num_workers,
+                                       pin_memory=True,
+                                       worker_init_fn=_worker_init_fn)
     return loader
-
-
-
-
-
-
-
